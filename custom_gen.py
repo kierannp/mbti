6634c4ece39da30dd775f8ffc115ce3a319de6cc
kierannp <nehilkieran@gmail.com>
2021-01-10 18:03:43 -0500

    just saving

--- custom_gen.py
+++ custom_gen.py
@@ -1,5 +1,5 @@
 import numpy as np
-import keras
+import tensorflow.keras as keras
 
 class DataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
@@ -23,13 +23,10 @@ class DataGenerator(keras.utils.Sequence):
         'Generate one batch of data'
         # Generate indexes of the batch
         indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
-
         # Find list of IDs
-        list_IDs_temp = [self.list_IDs[k] for k in indexes]
-
+        batch = [self.list_IDs[k] for k in indexes]
         # Generate data
-        X, y = self.__data_generation(list_IDs_temp)
-
+        X, y = self.__data_generation(batch)
         return X, y
 
     def on_epoch_end(self):
@@ -41,13 +38,13 @@ class DataGenerator(keras.utils.Sequence):
     def __data_generation(self, list_IDs_temp):
         'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
         # Initialization
-        X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        X = np.empty((self.batch_size, 512, 1))
         y = np.empty((self.batch_size), dtype=int)
 
         # Generate data
         for i, ID in enumerate(list_IDs_temp):
             # Store sample
-            X[i,] = np.load('data/' + ID + '.npy')
+            X[i,] = np.load( ID ).reshape((512,1))
 
             # Store class
             y[i] = self.labels[ID]
